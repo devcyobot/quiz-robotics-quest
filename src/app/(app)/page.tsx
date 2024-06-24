aa158fe30a29ee2bc72fb329d33931500044b104
tngPhVN <tnphan@ucsd.edu>
2024-06-24 16:59:01 -0700

    shrink container's width in screen larger 1280px

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -5,7 +5,7 @@ import Button from "../../packages/client/component/form/form-button/FormButton"
 const WelcomePage: FC = () => {
   return (
     <main className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
-      <section className="container bg-brand-purple-dark rounded-xl shadow-lg p-4 w-4/5 md:w-2/3 lg:w-1/2 max-h-400:h-[90%] h-2/3 sm:h-1/3 md:h-[55%] lg:h-1/2 max-h-[30rem] overflow-autoflex flex-col justify-between transform transition-all duration-300">
+      <section className="container bg-brand-purple-dark rounded-xl shadow-lg p-4 w-4/5 md:w-2/3 lg:w-1/2 xl:w-1/3 xl:min-w-[673px] max-h-400:h-[90%] h-2/3 sm:h-1/3 md:h-[55%] lg:h-1/2 max-h-[30rem] overflow-autoflex flex-col justify-between transform transition-all duration-300">
         <div className="h-full flex flex-col justify-evenly">
           <div className="mx-auto">
             <LogoBadge description={""} src={logoSrc.src} />
--- src/packages/client/component/form/EmailInvitation.tsx
+++ src/packages/client/component/form/EmailInvitation.tsx
@@ -53,7 +53,7 @@ const EmailInvitaion: FC = () => {
 
   return (
     <main className="fixed inset-0 z-50 flex flex-col items-center justify-center bg-black bg-opacity-50">
-      <section className="container bg-brand-purple-dark rounded-xl shadow-lg w-auto sm:w-2/3 lg:w-1/2 h-1/2 md:h-4/5 lg:h-[55%] max-h-[30rem] overflow-autoflex flex-col justify-between transform transition-all duration-300">
+      <section className="container bg-brand-purple-dark rounded-xl shadow-lg w-auto sm:w-2/3 lg:w-1/2 xl:w-1/3 xl:min-w-[673px] h-1/2 md:h-4/5 lg:h-[55%] max-h-[30rem] overflow-autoflex flex-col justify-between transform transition-all duration-300">
         <div className="h-full flex flex-col justify-evenly">
           <div className="mx-auto h-10">
             <LogoBadge description={""} src={logoSrc.src} />
